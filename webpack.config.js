192e0dd2c52e050e473b21a8267dd9b3b8fb4a36
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2020-09-06 22:08:33 +0200

    create project and header

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode:"production",
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "index.html")
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].[contentHash].css"
+      })],
+     module: {
+          rules: [
+            {
+              test: /\.scss$/,
+              use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                 "sass-loader"
+              ],
+            },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+              exclude: /node_modules/,
+              loader: 'file-loader',
+              options: {
+                name: "[name].[ext]",
+                outputPath: 'fonts/'
+              } 
+            }
+          ]
+        }, 
+  
+};
